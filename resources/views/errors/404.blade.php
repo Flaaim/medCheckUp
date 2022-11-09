d57a2c87eeee0297e4861993e0671fc1e525c416
flaaim <flaaim@list.ru>
2022-11-09 14:56:34 +1000

    fix 404 blade

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="column">
                 <div class="d-flex flex-column align-items-center mt-5">
-                    <h1>Страница на найдена</h1>
+                    <h1>Страница не найдена</h1>
                         <p>
                             Вернуться на <a href="{{route('home')}}">главную</a>
                         </p>
