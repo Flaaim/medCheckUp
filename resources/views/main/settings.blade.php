7d9ca4252eb7d6e6eae19efef08e1bc0067a045d
flaaim <flaaim@list.ru>
2022-11-10 12:20:19 +1000

    create model and migration

--- resources/views/main/settings.blade.php
+++ resources/views/main/settings.blade.php
@@ -7,6 +7,13 @@
     
     </div>
     <div class="card-body">
-
+        <h5>Импорт таблицы с факторами</h5>
+        <p>
+            Вы можете загрузить excel файл с вредными производственными факторами, устрановленными для профессий ваших работников, чтобы в дальнейшем при создании направления не указывать их вручную.
+        </p>
+        <form action="POST">
+            <label for="uploadExcel">Загрузить файл</label>
+            <input type="file" class="form-control" id="uploadExcel">
+        </form>
     </div>
 </div>
